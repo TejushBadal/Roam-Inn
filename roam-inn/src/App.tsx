e7d3cd3cdaf077d63e01b1b34c854be194a9189d
Mark-Mikaeel <mikaeelmark@gmail.com>
2024-11-14 23:29:03 -0500

    fix: :bug: add .tsx to import statement
    add '.tsx' to AdminLogin import

--- roam-inn/src/App.tsx
+++ roam-inn/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.tsx';
 import CreateAccount from './pages/CreateAccount.tsx';
 import LoginPage from './pages/LoginPage.tsx';
-import AdminLogin from './pages/AdminLogin';
+import AdminLogin from './pages/AdminLogin.tsx';
 
 const App: React.FC = () => {
     return (
